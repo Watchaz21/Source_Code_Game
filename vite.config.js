78ade507974efa4d9259384c965d6180272054e3
Watchaz21 <Watcharaphol.boot@gmail.com>
2025-09-03 23:44:29 +0700

    deploy path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Source_Code_Game/',
   build: {
     rollupOptions: {
       output: {
